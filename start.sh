0b7d9b4c4c8fc1f4966e0bf40302f88f6a4a4852
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-11 14:26:54 +0300

    Deploy to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY='6f9f7926b45b3e28bd348a9721c7f8a4'
 export SECRET_KEY='34265182'
+export MAIL_USERNAME='fitstargilii1194@gmail.com'
+export MAIL_PASSWORD='Gillie-2021'
 
 python3.8 manage.py server
